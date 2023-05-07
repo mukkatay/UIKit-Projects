27933541829782eada9142bd7e7920894be535e7
Akyl <akylmukatay@gmail.com>
2023-05-07 19:49:51 +0600

    add Link Collectors view controllers

---
+++ Project 00 - LinkCollector /BookmarkCollector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookmarkCollector
+//
+//  Created by Akyl Mukatay  on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
