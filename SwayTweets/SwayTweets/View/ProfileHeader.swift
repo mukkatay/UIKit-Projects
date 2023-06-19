b276b89a7a453c8b934975f87e25efcdc4c214de
Akyl <akylmukatay@gmail.com>
2023-06-19 15:01:10 +0600

    add Profile Header UI

--- SwayTweets/SwayTweets/View/ProfileHeader.swift
+++ SwayTweets/SwayTweets/View/ProfileHeader.swift
@@ -11,12 +11,100 @@ class ProfileHeader: UICollectionReusableView {
     
     //MARK: - Prooerties
     
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .menTweetRed
+        
+        view.addSubview(backButton)
+        backButton.anchor(top: view.topAnchor, left: view.leadingAnchor, paddingTop: 42, paddingLeft: 16)
+        backButton.setDimensions(width: 30, height: 30)
+        
+        return view
+    }()
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "arrow-left")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        return button
+    }()
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        iv.layer.borderColor = UIColor.white.cgColor
+        iv.layer.borderWidth = 4
+        return iv
+    }()
+    
+    private lazy var editProfileFollowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Follow", for: .normal)
+        button.layer.borderColor = UIColor.menTweetRed.cgColor
+        button.layer.borderWidth = 1.25
+        button.setTitleColor(.menTweetRed, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(handleEditProfileFollow), for: .touchUpInside)
+        return button
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "Bruce Lee"
+        return label
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .lightGray
+        label.text = "@brucelee"
+        return label
+    }()
+    
+    private let bioLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.numberOfLines = 3
+        label.text = "Lorem ipsum lorem gerol ipsum lorem gerol ipsum lorem gerol ipsum lorem gerol ipsum lorem gerol"
+        return label
+    }()
+    
     //MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        addSubview(containerView)
+        containerView.anchor(top: topAnchor, left: leadingAnchor, right: trailingAnchor, height: 108)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: containerView.bottomAnchor, left: leadingAnchor, paddingTop: -24, paddingLeft: 8)
+        profileImageView.setDimensions(width: 80, height: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: containerView.bottomAnchor,
+                                       right: trailingAnchor,
+                                       paddingTop: 12,
+                                       paddingRight: 12)
+        editProfileFollowButton.setDimensions(width: 100, height: 36)
+        editProfileFollowButton.layer.cornerRadius = 36 / 2
+        
+        let userDetailStack = UIStackView(arrangedSubviews: [fullnameLabel, usernameLabel, bioLabel])
+        userDetailStack.axis = .vertical
+        userDetailStack.distribution = .fillProportionally
+        userDetailStack.spacing = 4
+        addSubview(userDetailStack)
+        userDetailStack.anchor(top: profileImageView.bottomAnchor,
+                               left: leadingAnchor,
+                               right: trailingAnchor,
+                               paddingTop: 8,
+                               paddingLeft: 12,
+                               paddingRight: 12)
     }
     
     required init?(coder: NSCoder) {
@@ -24,6 +112,14 @@ class ProfileHeader: UICollectionReusableView {
     }
     
     //MARK: - Selectors
+    
+    @objc func handleDismissal() {
+        
+    }
+    
+    @objc func handleEditProfileFollow() {
+        
+    }
     
     //MARK: - Helpers
 }
