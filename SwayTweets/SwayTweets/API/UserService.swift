496522c959b363e51a6fefbd31b56128fb80f951
Akyl <akylmukatay@gmail.com>
2023-06-20 12:12:10 +0600

    add Following User

--- SwayTweets/SwayTweets/API/UserService.swift
+++ SwayTweets/SwayTweets/API/UserService.swift
@@ -6,6 +6,7 @@
 //
 
 import FirebaseAuth
+import FirebaseDatabase
 
 struct UserService {
     static let shared = UserService()
@@ -30,6 +31,14 @@ struct UserService {
             let user = User(uid: uid, dictionary: dictionary)
             users.append(user)
             completion(users)
+        }
+    }
+    
+    func followUser(uid: String, completion: @escaping(Error?, DatabaseReference) -> Void) {
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        
+        REF_USER_FOLLOWING.child(currentUid).updateChildValues([uid: 1]) { (err, ref) in
+            REF_USER_FOLLOWERS.child(uid).updateChildValues([currentUid: 1], withCompletionBlock: completion)
         }
     }
 }
--- SwayTweets/SwayTweets/Controller/ProfileController.swift
+++ SwayTweets/SwayTweets/Controller/ProfileController.swift
@@ -106,6 +106,12 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
 }
 
 extension ProfileController: ProfileHeaderDelegate {
+    func handleEditProfileFollow(_ header: ProfileHeader) {
+        UserService.shared.followUser(uid: user.uid) { (ref, err) in
+            
+        }
+    }
+    
     func handleDismissal() {
         navigationController?.popViewController(animated: true)
     }
