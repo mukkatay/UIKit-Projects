5098bcbbb3bd34206ca94147f891e7450cc5f9cc
Akyl <akylmukatay@gmail.com>
2023-06-21 16:47:59 +0600

    add Action Sheet Data Model + View Model

--- SwayTweets/SwayTweets/Utils/ActionSheetLauncher.swift
+++ SwayTweets/SwayTweets/Utils/ActionSheetLauncher.swift
@@ -16,6 +16,7 @@ class ActionSheetLauncher: NSObject {
     private let user: User
     private let tableView = UITableView()
     private var window: UIWindow?
+    private lazy var viewModel = ActionSheetViewModel(user: user)
     
     private lazy var blackView: UIView = {
         let view = UIView()
@@ -73,8 +74,6 @@ class ActionSheetLauncher: NSObject {
     //MARK: - Helpers
     
     func show() {
-        print(user.username)
-        
         guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
         self.window = window
         
@@ -82,7 +81,7 @@ class ActionSheetLauncher: NSObject {
         blackView.frame = window.frame
         
         window.addSubview(tableView)
-        let height = CGFloat(3 * 60) + 100
+        let height = CGFloat(viewModel.options.count * 60) + 100
         tableView.frame = CGRect(x: 0, y: window.frame.height,
                                  width: window.frame.width, height: height)
         
@@ -109,11 +108,12 @@ class ActionSheetLauncher: NSObject {
 
 extension ActionSheetLauncher: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return viewModel.options.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifire, for: indexPath) as! ActionSheetCell
+        cell.option = viewModel.options[indexPath.row]
         return cell
     }
 }
