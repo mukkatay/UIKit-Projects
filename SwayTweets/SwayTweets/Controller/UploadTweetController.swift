ee3dfe898a6d15f59f4d5b00876d336ca96ef61b
Akyl <akylmukatay@gmail.com>
2023-06-21 11:56:33 +0600

    use ViewModel To Populate Data

--- SwayTweets/SwayTweets/Controller/UploadTweetController.swift
+++ SwayTweets/SwayTweets/Controller/UploadTweetController.swift
@@ -41,6 +41,15 @@ class UploadTweetController: UIViewController {
         return iv
     }()
     
+    private lazy var replyLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.text = "Replying to @jstatham"
+        label.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        return label
+    }()
+    
     private let captionTextView = CaptionTextView()
     
     //MARK: - Lifecycle
@@ -94,10 +103,15 @@ class UploadTweetController: UIViewController {
         view.backgroundColor = .white
         configureNavigationBar()
         
-        let stack = UIStackView(arrangedSubviews: [profileImageView, captionTextView])
-        stack.axis = .horizontal
+        let imageCaptionStack = UIStackView(arrangedSubviews: [profileImageView, captionTextView])
+        imageCaptionStack.axis = .horizontal
+        imageCaptionStack.spacing = 12
+        imageCaptionStack.alignment = .leading
+        
+        let stack = UIStackView(arrangedSubviews: [replyLabel, imageCaptionStack])
+        stack.axis = .vertical
+//        stack.alignment = .leading
         stack.spacing = 12
-        stack.alignment = .leading
         
         view.addSubview(stack)
         stack.anchor(top: view.safeAreaLayoutGuide.topAnchor,
@@ -106,7 +120,16 @@ class UploadTweetController: UIViewController {
                      paddingTop: 16,
                      paddingLeft: 16,
                      paddingRight: 16)
+        
         profileImageView.sd_setImage(with: user.profileImageUrl)
+        
+        actionButton.setTitle(viewModel.actionButtonTitle, for: .normal)
+        captionTextView.placwholderLabel.text = viewModel.placeholderText
+        
+        replyLabel.isHidden = !viewModel.shouldShowReplyLabel
+        guard let replyText = viewModel.replyText else { return }
+        replyLabel.text = replyText
+                
     }
     
     func configureNavigationBar() {
