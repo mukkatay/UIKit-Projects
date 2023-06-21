c8bce83674626190b770d1925aecc3d9013be685
Akyl <akylmukatay@gmail.com>
2023-06-21 12:20:23 +0600

    uploading Reply To Firebase

--- SwayTweets/SwayTweets/API/TweetService.swift
+++ SwayTweets/SwayTweets/API/TweetService.swift
@@ -12,7 +12,7 @@ import FirebaseDatabase
 struct TweetService {
     static let shared = TweetService()
     
-    func uploadTweet(caption: String, completion: @escaping(Error?, DatabaseReference) -> Void) {
+    func uploadTweet(caption: String, type: UploadTweetConfiguration, completion: @escaping(Error?, DatabaseReference) -> Void) {
         guard let uid = Auth.auth().currentUser?.uid else { return }
         
         let values = ["uid": uid,
@@ -21,13 +21,17 @@ struct TweetService {
                       "retweets": 0,
                       "caption": caption] as [String : Any]
         
-        let ref = REF_TWEETS.childByAutoId()
-        
-        ref.updateChildValues(values) {(err, ref) in
-            // tweet обновление болғаннан кейін барып user-tweets обновлятся етеді
-            guard let tweetID = ref.key else { return }
-            REF_USER_TWEETS.child(uid).updateChildValues([tweetID: 1], withCompletionBlock: completion)
+        switch type {
+        case .tweet:
+            REF_TWEETS.childByAutoId().updateChildValues(values) {(err, ref) in
+                // tweet обновление болғаннан кейін барып user-tweets обновлятся етеді
+                guard let tweetID = ref.key else { return }
+                REF_USER_TWEETS.child(uid).updateChildValues([tweetID: 1], withCompletionBlock: completion)
+            }
+        case .reply(let tweet):
+            REF_TWEET_REPLIES.child(tweet.tweetID).childByAutoId().updateChildValues(values, withCompletionBlock: completion)
         }
+    
     }
     
     func fetchTweets(completion: @escaping([Tweet]) -> Void) {
