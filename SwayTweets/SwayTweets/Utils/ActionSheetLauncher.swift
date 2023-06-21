55174241204b5304acaf9e38437b50faf1de8ed3
Akyl <akylmukatay@gmail.com>
2023-06-21 15:35:09 +0600

    setting up Table View In Action Sheet

--- SwayTweets/SwayTweets/Utils/ActionSheetLauncher.swift
+++ SwayTweets/SwayTweets/Utils/ActionSheetLauncher.swift
@@ -7,21 +7,64 @@
 
 import UIKit
 
+private let reuseIdentifire = "ActionSheetCell"
+
 class ActionSheetLauncher: NSObject {
     
     //MARK: - Properties
     
     private let user: User
+    private let tableView = UITableView()
+    private var window: UIWindow?
     
     init(user: User) {
         self.user = user
         super.init()
+        
+        configureTableView()
     }
     
     //MARK: - Helpers
     
     func show() {
         print(user.username)
+        
+        guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
+        self.window = window
+        
+        window.addSubview(tableView)
+        tableView.frame = CGRect(x: 0, y: window.frame.height - 300,
+                                 width: window.frame.width, height: 300)
     }
+    
+    func configureTableView() {
+        tableView.backgroundColor = .red
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+        tableView.layer.cornerRadius = 5
+        tableView.isScrollEnabled = false
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifire)
+    }
+}
+
+//MARK: - UITableViewDataSource
+
+extension ActionSheetLauncher: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifire, for: indexPath)
+        return cell
+    }
+}
+
+//MARK: - UITableViewDelegate
+
+extension ActionSheetLauncher: UITableViewDelegate {
     
 }
