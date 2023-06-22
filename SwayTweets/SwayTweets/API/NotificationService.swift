ba3440ed5b8fa85bfbdb8e04f73b06463fd2b183
Akyl <akylmukatay@gmail.com>
2023-06-22 11:42:39 +0600

    add Like Notification

---
+++ SwayTweets/SwayTweets/API/NotificationService.swift
@@ -0,0 +1,28 @@
+//
+//  NotificationService.swift
+//  SwayTweets
+//
+//  Created by Akyl Mukatay  on 22.06.2023.
+//
+
+import FirebaseAuth
+import FirebaseDatabase
+
+struct NotificationService {
+    static let shared = NotificationService()
+    
+    func uploadNotification(type: NotificationType, tweet: Tweet? = nil) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        var values: [String: Any] = ["timestamp": Int(NSDate().timeIntervalSince1970),
+                                     "uid": uid,
+                                     "type": type.rawValue]
+        
+        if let tweet = tweet {
+            values["tweetID"] = tweet.tweetID
+            REF_NOTIFICATIONS.child(tweet.user.uid).childByAutoId().updateChildValues(values)
+        } else {
+            
+        }
+    }
+}
